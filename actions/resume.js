7acf0690aff08d3280f25744ea92e8012d9e0ff4
Abdul Samad <abdulsamad161803@gmail.com>
2025-09-10 08:45:25 +0530

    Update resume.js

--- actions/resume.js
+++ actions/resume.js
@@ -9,7 +9,7 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 
-export async function saveResume() {
+export async function saveResume(content) {
 
     const { userId } = await auth();
   if (!userId) {
